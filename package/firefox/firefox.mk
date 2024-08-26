c15fb3e6f510c0a5a60fcdee2feff8f2b278c47c
readysloth <readysloth@protonmail.com>
2024-08-26 09:34:53 +0300

    feat(package/firefox/firefox.mk): Added stripping of firefox binaries

--- package/firefox/firefox.mk
+++ package/firefox/firefox.mk
@@ -9,10 +9,14 @@ FIREFOX_LICENSE = MPL-2.0
 FIREFOX_SITE = https://archive.mozilla.org/pub/firefox/releases/$(FIREFOX_VERSION)/linux-$(BR2_ARCH)/en-US
 FIREFOX_VERSION = 129.0.1
 
+define FIREFOX_BUILD_CMDS
+	find $(@D) -exec file {} \; | grep -i elf | awk -F ':' '{print $$1}' | xargs $(STRIP)
+endef
+
 define FIREFOX_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/opt
 	cp -r $(@D)/* $(TARGET_DIR)/opt
-	ln -s /opt/firefox-bin $(TARGET_DIR)/usr/bin/firefox
+	ln -s /opt/firefox-bin $(TARGET_DIR)/usr/bin/firefox || true
 endef
 
 $(eval $(generic-package))
